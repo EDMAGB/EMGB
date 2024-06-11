7e3e9a52448e02e092e53a38e8265f23e5d1e127
EdnaGB <edna.garcia@mvs.com>
2024-06-11 15:10:00 -0600

    paso uno

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
