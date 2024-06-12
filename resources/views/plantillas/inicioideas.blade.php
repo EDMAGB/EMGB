95ab6656722c623df4d1e5afc593a55dcd73fadc
EdnaGB <edna.garcia@mvs.com>
2024-06-12 11:28:38 -0600

    css 2

--- resources/views/plantillas/inicioideas.blade.php
+++ resources/views/plantillas/inicioideas.blade.php
@@ -12,7 +12,16 @@
     <link rel="shortcut icon" href="https://i.ibb.co/GT8S9Yr/favicon.png">
 
     <!-- App css -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> --}}
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    @endphp
+
+    <!-- App css -->
+    <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/css/app.css']['file']) }}">
+
+    <!-- App js -->
+    <script src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
 
     <meta http-equiv="Expires" content="0">
     <meta http-equiv="Last-Modified" content="0">
@@ -20,139 +29,7 @@
     <meta http-equiv="Pragma" content="no-cache">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
-    <style>
-        body {
-            background-image: url('https://i.ibb.co/vP7BHzP/Comida-deliberacio-n.png'); 
-            background-size: cover;
-            background-position: center;
-            min-height: 100vh;
-        }
-
-        .card {
-            border: 1px solid #000000;
-            border-radius: 5px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
-            overflow: hidden;
-        }
-
-        .card-header {
-            border-radius: 30px 30px 0px 0px;
-            background: #000;
-            padding: 15px;
-            font-size: 1.25em;
-            text-align: center;
-        }
-
-        .card-body {
-            padding: 1.25rem;
-            flex: 1 1 auto;
-            fill: #F6F6F6;
-            filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-            color: #333;
-            font-family: Montserrat;
-            font-size: 15px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: normal;
-        }
-
-        .monRegular {
-            color: #222;
-            text-align: center;
-            font-family: Inter;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: normal;
-        }
-
-        .btnlogin {
-            border-radius: 100px;
-            border: 2px solid var(--TITLE, #4B66D3);
-            text-align: center;
-            text-decoration: none;
-            font-size: 12px;
-            margin: 4px 2px;
-            cursor: pointer;
-            display: flex;
-            width: 250px;
-            height: 35px;
-            padding: 6px 10px;
-            justify-content: center;
-            align-items: center;
-            flex-shrink: 0;
-            color: #000;
-            font-family: Inter;
-            font-size: 20px;
-            font-style: normal;
-            font-weight: 500;
-            line-height: normal;
-        }
-
-        .btnizquierda {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
-
-        .text-ques {
-            color: #FFF;
-            text-align: center;
-            font-family: Montserrat;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: normal;
-        }
-
-        a {
-            color: black;
-        }
-
-        a:hover {
-            color: inherit;
-        }
-
-        input {
-            font-family: Montserrat;
-            font-size: 15px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: normal;
-            color: #000;
-            border: 1px solid #000;
-            border-radius: 5px;
-            padding: 8px;
-            margin-bottom: 10px;
-        }
-
-        input::placeholder {
-            font-family: Montserrat;
-            font-size: 15px;
-            font-style: normal;
-            font-weight: 700;
-            line-height: normal;
-            color: #000;
-        }
-
-        input:-ms-input-placeholder {
-            color: #000;
-        }
-
-        input::-ms-input-placeholder {
-            color: #000;
-        }
-
-        .footer a {
-            color: #c2c0c0;
-            text-decoration: none;
-        }
-
-        .footer a:hover {
-            text-decoration: underline;
-        }
-    </style>
+  
 
 <body class="logofondo">
     @yield('contenido')
